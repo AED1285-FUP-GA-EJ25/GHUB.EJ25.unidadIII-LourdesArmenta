9df9af66b0bbfdead6f14acc5c2f7050e6fb13ad
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-03-31 17:59:07 +0000

    iniciamos con el paquete del problema2

---
+++ problema2/Condicion2.java
@@ -0,0 +1,21 @@
+package problema2;
+// Proposito: Leer un numero y si es positivo que manda el mensaje Positivo
+//            en caso contrario que mande el mensaje Negativo
+//            utilizando una funcion de nombre determinarPositivoNegativo
+//            esta funcion recibe como parametro un numero y si es positivo
+//            retorna el mensaje 'Positivo' y si es negativo retorna el 
+//            el mensaje 'Negativo'
+// Fecha:31 de marzo de 2025
+// Autor:Lourdes Armenta
+import java.util.Scanner;
+
+public class Condicion2{
+    public static void condicion2(){
+        //creo objeto Scanner
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+
+
+
+}
