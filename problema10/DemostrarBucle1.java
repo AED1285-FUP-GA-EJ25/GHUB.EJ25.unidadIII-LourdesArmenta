fcda8fd0ae5daa501e94d698e2e27e60ee25b312
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-04-29 17:40:19 +0000

    imprime Hola 10 veces

--- problema10/DemostrarBucle1.java
+++ problema10/DemostrarBucle1.java
@@ -2,7 +2,17 @@ package problema10;
 
 public class DemostrarBucle1 {
     public static void demostrarBubcle1(){
+        imprimirBucle();
         
+    }
+    public static void imprimirBucle(){
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println("Hola");
+
+        }
+        System.out.println("Termine");
+
     }
     
 }
